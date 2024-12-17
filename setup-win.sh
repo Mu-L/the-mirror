10147989c3c486620e0b0f652100306d5b441770
Gordon MacPherson <gordon@gordonite.tech>
2024-12-17 18:37:37 +0000

    crash fixes

--- setup-win.sh
+++ setup-win.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -o "MirrorGodotEditorWindows.exe" "https://storage.googleapis.com/mirror_native_client_builds/Engine/aac479fc/MirrorGodotEditorWindows.exe"
+curl -o "MirrorGodotEditorWindows.exe" "https://storage.googleapis.com/mirror_native_client_builds/Engine/497bbccc/MirrorGodotEditorWindows.exe"
