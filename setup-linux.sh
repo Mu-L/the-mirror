f6af81263dfc5b731e6226c6dd4ccce295aaf6d0
Gordon MacPherson <gordon@gordonite.tech>
2024-12-23 17:59:47 +0000

    Engine update for netsync message bug

--- setup-linux.sh
+++ setup-linux.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -o "MirrorGodotEditorLinux.x86_64" "https://storage.googleapis.com/mirror_native_client_builds/Engine/32231100/MirrorGodotEditorLinux.x86_64"
+curl -o "MirrorGodotEditorLinux.x86_64" "https://storage.googleapis.com/mirror_native_client_builds/Engine/13b5ad64/MirrorGodotEditorLinux.x86_64"
--- setup-win.sh
+++ setup-win.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -o "MirrorGodotEditorWindows.exe" "https://storage.googleapis.com/mirror_native_client_builds/Engine/32231100/MirrorGodotEditorWindows.exe"
+curl -o "MirrorGodotEditorWindows.exe" "https://storage.googleapis.com/mirror_native_client_builds/Engine/13b5ad64/MirrorGodotEditorWindows.exe"
